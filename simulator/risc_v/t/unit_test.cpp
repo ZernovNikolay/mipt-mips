c6b1989948421450704dd50775b8051b53724be4
ZernovNikolay <zernov.na@phystech.edu>
2019-11-17 22:44:03 +0300

    Added a new instruction pcnt

--- simulator/risc_v/t/unit_test.cpp
+++ simulator/risc_v/t/unit_test.cpp
@@ -297,6 +297,7 @@ TEST_CASE( "RISV RV64 xnor")
 
 TEST_CASE("RISCV RV32 bfp")
 {
+
     CHECK( RISCVInstr<uint32>(0x08F77833).get_disasm() == "bfp $a6, $a4, $a5");
     RISCVInstr<uint32> instr( "bfp", 0);
     instr.set_v_src( 0x5555, 0);
@@ -312,4 +313,21 @@ TEST_CASE("RISCV RV64 bfp")
     instr.set_v_src( 0x081C00C5, 1); //len = 8, off = 28, data = 1100'0101
     instr.execute();
     CHECK( instr.get_v_dst() == 0x0000'555C'5CCC'0000);
+}
+
+TEST_CASE("RISCV RV32 pcnt")
+{
+    CHECK( RISCVInstr<uint32>(0x60281593).get_disasm() == "pcnt $a1, $a6, $sp");
+    RISCVInstr<uint32> instr( "pcnt", 0);
+    instr.set_v_src( 0xfff, 0);
+    instr.execute();
+    CHECK( instr.get_v_dst() == 0xc);
+}
+
+TEST_CASE("RISCV RV64 pcnt")
+{
+    RISCVInstr<uint64> instr( "pcnt", 0);
+    instr.set_v_src( 0xfafb'fcfd'feff, 0);
+    instr.execute();
+    CHECK( instr.get_v_dst() == 41);
 }
